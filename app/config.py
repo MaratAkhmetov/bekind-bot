59f38a0d7f92aee685257c405c84a3e384ca851a
Marat Akhmetov <makhmet1992@gmail.com>
2026-05-09 21:19:25 +0200

    new version with new architecture

--- app/config.py
+++ app/config.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 PERPLEXITY_API_KEY = os.getenv("PERPLEXITY_API_KEY")
 
-DATABASE_PATH = "bekind.db"
+DATABASE_PATH = os.getenv("DATABASE_PATH", "app/database/db.sqlite")
